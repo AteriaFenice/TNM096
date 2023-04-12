705be947c8188002ba5b218e7106dd0ab970beb2
Malva <halva.malva@gmail.com>
2023-04-12 15:24:27 +0200

    mayor change

--- main.py
+++ main.py
@@ -6,88 +6,221 @@
 from itertools import chain
 from queue import PriorityQueue
 import time
+import copy
 
 heuristics = input('Enter heuristic method: \nh1: Misplaced tiles \nh2: Manhattan Distance\n')
 
 ''' Define one Node and generate child states from the current state '''
 class Node: 
     ''' Initialize a node '''
-    def __init__(self, puzzle, level, f):
+    def __init__(self, puzzle, level, f, parent = None):
         # Initialize the node
         self.puzzle = puzzle # 
         self.level = level # level in the tree
         self.f = f # h+g (h = nr of misplaced tiles, g = the path cost)
+        self.parent = parent
 
-    def __lt__(self, other):
+    def __lt__(self, other): # less than operator
         return self.f < other.f
-
-    ''' Generate child nodes by moving all possible directions (moving blank space) '''
-    def get_child(self):
-        x, y = self.find(self.puzzle,'_') # find blank space
-        positions = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]] # positions to move blank space in the 4 directions
-        children = []
-
-        for i in positions:
-            child = self.move(self.puzzle, x, y, i[0], i[1]) # i[0]=x pos of child, i[1]=y pos of child, move blank to that position
-            if child is not None: # make node and add to children
-                child_node = Node(child, self.level+1, 0)
-                children.append(child_node)
-        return children
-                
-
-    ''' Move the blank space in the given direction and of the position value are out
-        of limits then return None '''  
-    def move(self, puzzle, x1, y1, x2, y2): 
-        if x2 >= 0 and x2 <len(self.puzzle) and y2 >= 0 and y2 < len(self.puzzle):
-            # Allocating memory to move the tiles
-            temp_puzzle = [] 
-            temp_puzzle = self.copy(puzzle) # copy the current puzzle
-            temp = temp_puzzle[x2][y2] # Position of what we want to move the blank space too
-            temp_puzzle[x2][y2] = temp_puzzle[x1][y1] # Gives the current blank the child 
-            temp_puzzle[x1][y1] = temp # Moves the blank to the child 
-            return temp_puzzle # returns the puzzle with the new moved tiles
-        else:
-            return None # If trying to move the empty tile out of bounds return none
-        
-    ''' Copy function to create a copy of the current puzzle matrix '''
-    def copy(self, root): # root of the puzzle that is the input
-        temp = [] 
-        for i in root: 
-            t = []
-            for j in i:
-                t.append(j) # append node to row
-            temp.append(t) # append row to matrix
-
-        return temp
     
-    ''' Returns position of blank space '''
-    def find(self, puzzle, blank):
-        for i in range(0, len(self.puzzle)): 
-            for j in range(0, len(self.puzzle)):
-                if puzzle[i][j] == blank:
-                    return i, j
+    '''Heurisitc function to calculate heuristic value f(x) = h(x) + g(x)'''
+    def cal_f(self, start, goal):
+        if heuristics == 'h1': 
+          return self.cal_h1(start.puzzle, goal) + start.level
+        
+        if heuristics == 'h2':
+          return self.cal_h2(start.puzzle, goal) + start.level
+        
+    ''' Calcualtes the different between the given puzzles '''
+    def cal_h1(self, start, goal):
+        counter = 0
+        for i in range(0, 3): #row
+            for j in range(0, 3): #col
+                if start[i][j] != goal[i][j] and start[i][j] != '_':
+                    counter += 1 # Counts all squares that are in the wrong position
+        return counter
 
-    ''' ????? '''       
-    def get_key(self):
-        flatten_list = list(chain.from_iterable(self.puzzle))
-        result = ''.join(str(e) for e in flatten_list)
+    ''' Manhattan Distance '''
+    def cal_h2(self, start, goal):
+        counter = 0
 
-        return result
-   
+        for i in range(0, 3): #row
+            for j in range(0, 3): #col
+                if start[i][j] != goal[i][j] and start[i][j] != '_':
+
+                    for x in range(0, 3): 
+                        for y in range(0, 3):
+                            if goal[x][y] == start[i][j]:
+                                x_pos = x
+                                y_pos = y
+                                break
+
+                    # calculate distance 
+                    manhattan = abs(i-x_pos) + abs(j-y_pos)
+
+                    counter += manhattan # Counts all manhattan distances of squares that are in the wrong position 
+
+        return counter
+
+''' Generate child nodes by moving all possible directions (moving blank space) '''
+def get_child(self):
+    x, y = find(self.puzzle,'_') # find blank space
+    positions = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]] # positions to move blank space in the 4 directions
+    children = []
+
+    for i in positions:
+        child = move(self.puzzle, x, y, i[0], i[1]) # i[0]=x pos of child, i[1]=y pos of child, move blank to that position
+        if child is not None: # make node and add to children
+            #child_node = Node(child, self.level+1, 0)
+            children.append(child)
+    return children
+
+''' Returns position of blank space '''
+def find(puzzle, blank):
+    for i in range(0, len(puzzle)): 
+        for j in range(0, len(puzzle)):
+            if puzzle[i][j] == blank:
+                return i, j
+            
+
+''' Move the blank space in the given direction and of the position value are out
+    of limits then return None '''  
+def move(puzzle, x1, y1, x2, y2): 
+    if x2 >= 0 and x2 <len(puzzle) and y2 >= 0 and y2 < len(puzzle):
+        # Allocating memory to move the tiles
+        temp_puzzle = [] 
+        temp_puzzle = copy.deepcopy(puzzle) # copy the current puzzle
+        temp = temp_puzzle[x2][y2] # Position of what we want to move the blank space too
+        temp_puzzle[x2][y2] = temp_puzzle[x1][y1] # Gives the current blank the child 
+        temp_puzzle[x1][y1] = temp # Moves the blank to the child 
+        return temp_puzzle # returns the puzzle with the new moved tiles
+    else:
+        return None # If trying to move the empty tile out of bounds return none
+
+''' Copy function to create a copy of the current puzzle matrix '''
+'''def copy(root): # root of the puzzle that is the input
+    temp = [] 
+    for i in root: 
+        t = []
+        for j in i:
+            t.append(j) # append node to row
+        temp.append(t) # append row to matrix
+
+    return temp'''
+
+
+
+''' ????? '''       
+'''def get_key(self):
+    flatten_list = list(chain.from_iterable(self.puzzle))
+    result = ''.join(str(e) for e in flatten_list)
+
+    return result'''
+    
+    
+def a_star(start, goal, h):
+
+    start = Node(start,0,0)
+    #goal = Node(goal)
+
+    open = PriorityQueue()
+    open.put(start)
+
+    closed = set()
+
+    while not open.empty():
+        current = open.get()
+        
+        if current.puzzle == goal:
+            path = []
+            while current.parent is not None:
+                path.append(current.puzzle)
+                current = current.parent
+            path.append(start)
+            path.reverse()
+            return path
+        
+        closed.add(tuple(map(tuple,current.puzzle)))
+
+        for child in get_child(current):
+            child_puzzle = tuple(map(tuple, child))
+
+            if child_puzzle not in closed:
+                child_node = Node(child, level=current.level+1, f=current.cal_f(start, goal), parent = current)
+                open.put(child_node)
+
+    return None
+
+'''Accepts puzzle from user'''
+def define_puzzle():
+    puz = []
+    for i in range(0, 3):
+        temp = input().split(' ')
+        puz.append(temp)
+    return puz
+
+
+print('Enter puzzle at start state:')
+start = define_puzzle()
+# Enter own goal state
+#print('Enter puzzle at goal state')
+#goal = self.define_puzzle()
+
+#heuristics = heuristics.replace(' ', '')
+
+#print('h: ', heuristics)
+
+# Pre-defined goal, increasing order
+goal = [['1', '2', '3'], ['4', '5', '6'],['7', '8', '_']]
+#print("\ngoal: ", goal)
+print('\nGoal State, increasing order:')
+print("1 2 3\n4 5 6\n7 8 _")
+
+path = a_star(start, goal, heuristics)
+
+if path is not None:
+    print('Number of moves:', len(path)-1)
+    '''for i, puzzle in enumerate(path):
+        print(f'Step {i}:')
+        for row in puzzle:
+            print(row)
+    '''
+    counter = 0
+    for i in path:
+        print('Step: ', counter)
+        print(i)
+        print()
+        counter += 1
+else: 
+    print('no solution found')
+
+#print('solved puzzle!')
+#print('nr of iterations: ', it)
+
+'''path = a_star(start_state, goal_state, h2)
+    if path is not None:
+        print('Number of moves:', len(path)-1)
+        for i, state in enumerate(path):
+            print(f'Step {i}:')
+            for row in state:
+                print(row)
+            print()'''
+        
+
     
 ''' Accepts the initial goal states of the N-Puzzle problem
     and provides the functions to calculate the f-score of any given node(state) '''
-class Puzzle:
+'''class Puzzle:
 
-    ''' Initialize the puzzle ''' 
+    Initialize the puzzle
     def __init__(self, size):
         self.n = size # Size of the puzzle
         self.open = [] # Initialize the open list to empty
         self.closed = [] # Intialize the closed list to empty
-        '''self.open = PriorityQueue()
-        self.closed = {}'''
+        self.open = PriorityQueue()
+        self.closed = {}
         
-    ''' Accepts puzzle from user '''
+    Accepts puzzle from user
     def define_puzzle(self):
         puz = []
         for i in range(0, self.n):
@@ -95,7 +228,7 @@ class Puzzle:
             puz.append(temp)
         return puz
     
-    ''' Heurisitc function to calculate heuristic value f(x) = h(x) + g(x) '''
+ Heurisitc function to calculate heuristic value f(x) = h(x) + g(x)
     def cal_f(self, start, goal):
         if heuristics == 'h1': 
           return self.cal_h1(start.puzzle, goal) + start.level
@@ -103,17 +236,16 @@ class Puzzle:
         if heuristics == 'h2':
           return self.cal_h2(start.puzzle, goal) + start.level
 
-    ''' Calcualtes the different between the given puzzles '''
+    Calcualtes the different between the given puzzles
     def cal_h1(self, start, goal):
         counter = 0
         for i in range(0, self.n): #row
             for j in range(0, self.n): #col
                 if start[i][j] != goal[i][j] and start[i][j] != '_':
                     counter += 1 # Counts all squares that are in the wrong position
-
         return counter
 
-    ''' Manhattan Distance '''
+    Manhattan Distance
     def cal_h2(self, start, goal):
         counter = 0
 
@@ -135,7 +267,7 @@ class Puzzle:
 
         return counter
         
-    ''' Accept start and goal puzzle state''' 
+    Accept start and goal puzzle state
     def solve_puzzle(self):
         print('Enter puzzle at start state:')
         start = self.define_puzzle()
@@ -216,9 +348,9 @@ class Puzzle:
                     self.open.append(i)
                     #print('add node to open')
                 
-                '''while not self.open.empty():
+                while not self.open.empty():
                     if i.puzzle not in self.open.get() and i.puzzle not in self.closed.get():
-                        self.open.put(i)'''
+                        self.open.put(i)
                 
 
             self.closed.append(current)
@@ -242,6 +374,8 @@ puz.solve_puzzle()
 
 et = time.process_time()
 
-print('time: ', et-st, 'seconds')
+print('time: ', et-st, 'seconds')'''
+
+
 
 # make heap queue
