c624e514ccf403a7290b861d284263d29eb030a7
Maria <maria.brunned@gmail.com>
2023-04-27 14:48:11 +0200

    some changes

--- lab3/main.py
+++ lab3/main.py
@@ -2,22 +2,49 @@
 # Authors: Maria & Malva
 
 import random
+import copy
 
 class Clause: 
     def __init__(self, p, n):
-        self.p = p
-        self.n = n 
+        self.p = set(p)
+        self.n = set(n) 
     
     def __str__(self):
-        return str(self.p) + " " + str(self.n)
+        return "p: " + str(self.p) + ", n: " + str(self.n)
 
     def __repr__(self):
-        return str(self.p) + " " + str(self.n)
+        return "p: " + str(self.p) + ", n: " + str(self.n)
+    
+    def __hash__(self):
+    #freezes a set so it doesnt change, 
+    #remains the same, frozensets can be used as keys
+        return hash((frozenset(self.p), frozenset(self.n)))
+    
+      # equal between two hash values
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.p == other.p and self.n == other.n
 
 
-def resolution(A, B): 
+    def is_subset(self, other):
+        #return self.p.issubset(other.p) and self.n.issubset(other.n)
+        if self.p <= other.p and self.n <= other.n:
+         return True
+        
+        return False
+    
+    def is_strict_subset(self, other):
+        if not self.is_subset(other):
+            return False
+        if self.p < other.p or self.n < other.n:
+            return True
+        return False
+
+
+def resolution(A, B): # A: clause, B: clause
     #print('A = ', A, ', B = ', B)
-    # & -> intersection
+
     if not (A.p.intersection(B.n)) and not (A.n.intersection(B.p)):
         return False # There is no solution (no intersection)
     
@@ -44,54 +71,57 @@ def solver(KB):
 
     KB_prim = set()
 
-    while(KB_prim != KB):
+    while (KB_prim != KB):
+        
         S = set()
-        KB_prim = KB 
+        KB = copy.deepcopy(KB)
+        #KB_prim = copy.deepcopy(KB) 
 
         #print('KB: ', KB)
 
         #for A,B in enumerate(KB): 
         for A in KB: 
             for B in KB:
-                #print('A = ', A, ', B = ', B)
                 C = resolution(A, B)
-                if C != False:
-                    S = S.union({C})
-
+                if C is not False:
+                    S = S | set({C})
+        
         if not S:
+            print('hello?')
             return KB
         
+        print('hello???')
+
         KB = incorporate(S, KB)
+        
+        print('KB: ')
+        for C in KB: 
+            print(C)
 
-    print("KB: ")
-    for C in KB: 
-        print(C)
-    print("S: ")
-    for C in S:
-        print(C)
-    print()
-
+        print('S: ')
+        for C in S:
+            print(C)
+        print()
+    
     return KB
 
 
-def incorporate(S, KB):
+def incorporate(S, KB): # S: set of clauses, KB: set of clauses
     for A in S:
         KB = incorporate_clause(A, KB)
-
     return KB
 
-def incorporate_clause(A, KB):
-
+def incorporate_clause(A, KB): # A: clause, KB: Set of clauses
     for B in KB:
-        if set(B.p).issubset(A.p) and set(B.n).issubset(A.n):
+        if B.is_subset(A):
             return KB
     
-    for B in KB: 
-        if set(A.p).issubset(B.p) and set(A.n).issubset(B.n):
+    for B in copy.deepcopy(KB): 
+        if A.is_subset(B):
             KB.remove(B)
 
-    KB = KB.union(A)
-    return 
+    KB = KB.union(set({A}))
+    return KB
 
 
 # Example 1 - Resolution
@@ -113,6 +143,38 @@ B3 = Clause(p = {'z', 'b'}, n = {'c'})
 result3 = resolution(A3, B3)
 print('Ex 3: ', result3)
 
+# Subsumption
+# 1.
+A5 = Clause(p = {'c', 'a'}, n = {})
+B5 = Clause(p = {'a', 'b', 'c'}, n = {})
+result5 = A5.is_strict_subset(B5)
+print('Strict subset 1: ', result5)
+
+# 2.
+A5 = Clause(p = {'b'}, n = {'c'})
+B5 = Clause(p = {'a', 'b'}, n = {'c'})
+result5 = A5.is_strict_subset(B5)
+print('Strict subset 2: ', result5)
+
+# 3.
+A5 = Clause(p = {'b'}, n = {'f', 'c'})
+B5 = Clause(p = {'a', 'b'}, n = {'c'})
+result5 = A5.is_strict_subset(B5)
+print('Strict subset 3: ', result5)
+
+# 4. 
+A5 = Clause(p = {'b'}, n = {})
+B5 = Clause(p = {'a', 'b'}, n = {'c'})
+result5 = A5.is_strict_subset(B5)
+print('Strict subset 4: ', result5)
+
+# 5. 
+A5 = Clause(p = {'b', 'a'}, n = {'c'})
+B5 = Clause(p = {'a', 'b'}, n = {'c'})
+result5 = A5.is_strict_subset(B5)
+result6 = A5.is_subset(B5)
+print('Strict Subset 5: ', result5)
+
 # Drawing conclusion
 # Bob.
 ice = 'a'
@@ -128,7 +190,9 @@ E4 = Clause(p = {'sun', 'money', 'cry'}, n = {})
 F4 = Clause(p = {'movie'}, n = {})
 
 KB = set({A4, B4, C4, D4, E4, F4})
-print('KB: ', KB)
 result4 = solver(KB)
-print('solver finished')
-print('KB: ', result4)
+#print('KB: ', result4)
+print('\nFinal Clauses: ')
+for C in result4: 
+    print(C)
+
