75376750ae23f9dff9ef20d51dc360d580a9198f
Maria <maria.brunned@gmail.com>
2023-04-27 10:45:19 +0200

    adding test with bob

--- lab3/main.py
+++ lab3/main.py
@@ -3,25 +3,37 @@
 
 import random
 
-def resolution(A, B): 
+class Clause: 
+    def __init__(self, p, n):
+        self.p = p
+        self.n = n 
+    
+    def __str__(self):
+        return str(self.p) + " " + str(self.n)
 
+    def __repr__(self):
+        return str(self.p) + " " + str(self.n)
+
+
+def resolution(A, B): 
+    #print('A = ', A, ', B = ', B)
     # & -> intersection
-    if not A.p & B.n and not A.n & B.p:
+    if not (A.p.intersection(B.n)) and not (A.n.intersection(B.p)):
         return False # There is no solution (no intersection)
     
-    if (A.p & B.n):
-        a = random.choice(A.p & B.n)
+    if (A.p.intersection(B.n)):
+        a = random.choice(list(A.p.intersection(B.n)))
         A.p.remove(a) # Removes element a from the set if its in the intersection
         B.n.remove(a)
     else:
-        a = random.choice(A.n & B.p)
+        a = random.choice(list(A.n.intersection(B.p)))
         A.n.remove(a) 
         B.p.remove(a)
 
-    C = set()
-    C.p.add(A.p.union(B.p)) # Adds the union 
-    C.n.add(A.n.union(B.n))
-    if C.p & C.n: # C is tautology(always true)
+    C = Clause(p = set(A.p).union(B.p), n = set(A.n).union(B.n))
+    #C.p.add(A.p.union(B.p)) # Adds the union 
+    #C.n.add(A.n.union(B.n))
+    if C.p.intersection(C.n): # C is tautology(always true)
         return False
 
     # C is a set so it should not have any duplicates by default
@@ -36,52 +48,87 @@ def solver(KB):
         S = set()
         KB_prim = KB 
 
-        for A,B in KB: 
-            C = resolution(A, B)
-            if C != False:
-                S.add(S.union({C}))
+        #print('KB: ', KB)
+
+        #for A,B in enumerate(KB): 
+        for A in KB: 
+            for B in KB:
+                #print('A = ', A, ', B = ', B)
+                C = resolution(A, B)
+                if C != False:
+                    S = S.union({C})
 
         if not S:
             return KB
         
-        KB.add(incorporate(S, KB))
+        KB = incorporate(S, KB)
+
+    print("KB: ")
+    for C in KB: 
+        print(C)
+    print("S: ")
+    for C in S:
+        print(C)
+    print()
+
+    return KB
 
 
 def incorporate(S, KB):
-
     for A in S:
-        KB.add(incorporate_clause(A, KB))
+        KB = incorporate_clause(A, KB)
 
     return KB
 
 def incorporate_clause(A, KB):
+
     for B in KB:
-        if B.p.issubset(A.p) and B.n.issubset(A.n):
+        if set(B.p).issubset(A.p) and set(B.n).issubset(A.n):
             return KB
     
     for B in KB: 
-        if A.p.issubset(B.p) and A.n.issubset(B.n):
+        if set(A.p).issubset(B.p) and set(A.n).issubset(B.n):
             KB.remove(B)
 
-    KB.add(KB.union(A))
+    KB = KB.union(A)
     return 
 
 
 # Example 1 - Resolution
 # 1.
+A1 = Clause(p = {'a', 'b'}, n ={'c'} )
+B1 = Clause(p = {'c', 'b'}, n = {})
+result1 = resolution(A1,B1)
+print('Ex 1: ', result1)
 
-a_p = {'a', 'b'}
-b_n = {'c'}
+# 2.
+A2 = Clause(p = {'a', 'b'}, n ={'c'}) 
+B2 = Clause(p = {'d', 'b'}, n = {'g'})
+result2 = resolution(A2,B2)
+print('Ex 2: ', result2)
 
-A = 
+# 3.
+A3 = Clause(p = {'c', 't'}, n = {'b'})
+B3 = Clause(p = {'z', 'b'}, n = {'c'})
+result3 = resolution(A3, B3)
+print('Ex 3: ', result3)
 
-B.p = {'c', 'b'}
-B.n = {}
+# Drawing conclusion
+# Bob.
+ice = 'a'
+sun = 'b'
+money = 'c'
+movie = 'd'
+cry = 'e'
+A4 = Clause(p = {'ice'}, n = {'sun', 'money'})
+B4 = Clause(p = {'ice', 'movie'}, n = {'money'})
+C4 = Clause(p = {'money'}, n = {'movie'})
+D4 = Clause(p = {}, n = {'movie', 'ice'})
+E4 = Clause(p = {'sun', 'money', 'cry'}, n = {})
+F4 = Clause(p = {'movie'}, n = {})
 
-
-
-result = resolution(A,B)
-
-print(result)
-
-
+KB = set({A4, B4, C4, D4, E4, F4})
+print('KB: ', KB)
+result4 = solver(KB)
+print('solver finished')
+print('KB: ', result4)
